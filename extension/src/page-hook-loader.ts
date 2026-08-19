0c2605663556978fd97fe471e2d1216e0761988b
Rajeet <112367119+rajeet-04@users.noreply.github.com>
2026-08-19 10:20:58 +0530

    feat: add Firefox page-world hook loader

---
+++ extension/src/page-hook-loader.ts
@@ -0,0 +1,5 @@
+const script = document.createElement("script");
+script.src = chrome.runtime.getURL("page-submission-hook.js");
+script.async = false;
+script.addEventListener("load", () => script.remove(), { once: true });
+(document.head || document.documentElement).append(script);
